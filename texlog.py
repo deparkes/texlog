91ba1c8f3638e3559e3e26c41cb52028534c94a8
Duncan Parkes <ppxdep@nottingham.ac.uk>
2015-02-20 16:26:12 +0000

    Work towards extracting all sections

--- texlog.py
+++ texlog.py
@@ -9,10 +9,10 @@ Output to a simple text file that can be plotted with your favourite plotting
 software.
 
 TODO:
-    - Break up into functions
-    - Run texcount from this script
     - Capture output from texcount run
     - Automatically plot with matplotlib?
+    - Have command line capability: input file, output file, optparser
+    - Option to store more than just the total
     
 
 """
@@ -21,14 +21,19 @@ TODO:
 from datetime import datetime
 import os
 import subprocess
-
+#
+#class section:
+#    def __init__(self):
 
 def run_texcount(folder, texfile):
+    """ Run texcount on a specified file in a specified directory.
+        Output the standard texcount output
+    """
     texcount = []
     os.chdir(folder)
     print os.getcwd()
     tex_count_command = "texcount -inc " + texfile
-    print tex_count_command
+    print "Running: ", tex_count_command
 #    subprocess.call(tex_count_command, shell=True)
     proc = subprocess.Popen(tex_count_command,stdout=subprocess.PIPE)
     while True:
@@ -41,28 +46,61 @@ def run_texcount(folder, texfile):
     
     return texcount
 def get_tex_total(countfile):
+    """ Extract relevant section of the texcount output.
+        I couldn't figure out how to do exactly what I """
+    IncludedLineLimit = 0    
+    FilesTotalLineLimit = 0    
+    extract1 = False
+    extract2 = False
     block_of_lines = []
+#    with open(countfile) as input_data:
+    # Skips text before the beginning of the interesting block:
+    for line in countfile:
+#        print line
+#        type(line)
 
-    with open(countfile) as input_data:
-        # Skips text before the beginning of the interesting block:
-        for line in input_data:
-            if "total" in line.strip():  # Or whatever test is needed
-                break
-        # Reads text until the end of the block:
-        for line in input_data:  # This keeps reading the file
-            if line.strip() == 'Subcounts:':
-                break
+        if "Included file:" in line.strip()[:20]:  # Or whatever test is needed
+            extract1 = True       
+        if extract1:
             print line
-            block_of_lines.append(line.strip('\n'))  # Line is extracted (or block_of_lines.append(line), etc.)
+            IncludedLineLimit +=1 
+        if IncludedLineLimit > 8:
+            extract1 = False
+            IncludedLineLimit = 0
             
+        if "File(s) total:" in line.strip()[:20]:
+            extract2 = True       
+        if extract2:
+            print line
+            FilesTotalLineLimit +=1 
+        if FilesTotalLineLimit > 8:
+            extract2 = False
+            FilesTotalLineLimit = 0              
+            
+#            if "Words in text" in line.strip()[:20]:
+##            print line
+#                extract = True
+#            if 'Number of math displayed'  in line.strip()[:26]:
+#                extract = False
+#            elif extract:
+#                print line
+##            if not "total" in line.strip():
+#                block_of_lines.append(line.strip())  # Line is extracted (or block_of_lines.append(line), etc.)
+#        else:
+#            break
     return block_of_lines
     
-def logger(countfile):
-    
+def logger(folder, tex_file):
+
     
     time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M")
-    
-    block_of_lines = get_tex_total(countfile)
+#    if os.path.isfile(countfile):
+#        with open(countfile) as input_data:
+#            for line in input_data:
+#                texcount.append(line.rstrip())
+#    else:
+    texcount = run_texcount(folder, tex_file)
+    block_of_lines = get_tex_total(texcount)
             
     #print block_of_lines
     
@@ -72,13 +110,17 @@ def logger(countfile):
 #    print time_stamp
     
     for line in block_of_lines:
-        (heading, value) = line.split(':')
-        headers.append(heading)
-        values.append(value)
+        print line
+        try:
+            (heading, value) = line.split(':')
+            headers.append(heading)
+            values.append(value)
+        except Exception as e:
+            print "Problem with ", line
+            print "Error:", e
     
     #print headers
-    #print values
-    
+#    print values
     
     
     output_file = "output.txt"
@@ -86,12 +128,13 @@ def logger(countfile):
         if os.stat(output_file).st_size == 0:
             f.writelines(",".join(map(str, headers)))
             f.writelines(",".join('\n'))
+            
         f.writelines(",".join(map(str, values)))
         f.writelines(",".join('\n'))
 
 
 tex_file = "thesis.tex"
 folder = "Z:\\Backup\\thesis"
-run_texcount(folder, tex_file)
-countfile = "Z:\\Backup\\thesis\\texcount_output.txt"
-logger(countfile)
+#run_texcount(folder, tex_file)
+#countfile = "Z:\\Backup\\thesis\\texcount_output.txt"
+logger(folder, tex_file)
