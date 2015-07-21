6b4e87cd374e7765daa2e8de850cdd33974cff11
Duncan Parkes <ppxdep@nottingham.ac.uk>
2015-07-21 13:56:47 +0100

    Create files for module

---
+++ setup.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 12:16:00 2015
+
+@author: ppxdep
+"""
+
---
+++ texlog/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 12:15:22 2015
+
+@author: ppxdep
+"""
+
