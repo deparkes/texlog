30cbc3c8173408ea4ea6087895607d695903de83
Duncan Parkes <deparkes@gmail.com>
2019-08-10 10:25:57 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='texlog',
-      version='0.3.0',
+      version='0.4.0',
       description='Log the wordcount of tex files',
       url='https://github.com/deparkes/texcount',
       author='deparkes',
